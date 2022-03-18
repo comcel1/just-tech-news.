df49152b5e50ee9825d175c964e730d4518b063b
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-03-18 11:45:10 -0500

    created the comment model

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
+// By also creating one-to-many associations directly between these models, we can perform aggregated SQL functions between models. In this case, we'll see a total count of votes for a single post when queried. This would be difficult if we hadn't directly associated the Vote model with the other two.
 User.hasMany(Post, {
   foreignKey: 'user_id',
 });
@@ -27,6 +29,22 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id',
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
 // These two methods allow the USER and POST to query each other's information
 User.belongsToMany(Post, {
   through: Vote,
@@ -40,4 +58,4 @@ Post.belongsToMany(User, {
   foreignKey: 'post_id',
 });
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
